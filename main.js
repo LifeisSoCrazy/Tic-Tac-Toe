57edfa3e5cc60df385231635156d903ff02c7e68
LifeisSoCrazy <rahmanmohammed51@gmail.com>
2022-10-12 02:33:06 -0400

    Finally got most of the user interface with regards to the actual game of tic-tac-toe

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 // displayController(); the function used to display the function onto the tic-tac-toe board
 // gameBoard(): the function used to implement the correct logic
+
 const player = document.getElementsByClassName("player");
 const positions = Array.from(document.getElementsByClassName("position"));
 const reset = document.getElementsByClassName("reset")
+const choices = Array.from(document.querySelectorAll(".choice"));
+
 
 positions.forEach(position => {
-  position.addEventListener('click', () =>
-    game_board.update_board(position));
+  position.addEventListener('click', () => {
+    game_board.update_board(position);
+    console.log(position.innerText)
+  })});
+
+
+first_move = ''
+
+
+choices.forEach(choice => {
+  choice.addEventListener('click', () => {
+    first_move = choice.innerText;
+    console.log(first_move)
+  });
 })
 
 const game_board = (() => {
@@ -15,24 +30,33 @@ const game_board = (() => {
       position.innerText = '';
     });
   };
+
   
+
   const update_board = 
-      // return letter based on players turn
-      (e) => {e.innerText = 'x'}
-  const player_turn = function() {
+    // return letter based on players turn
+    (e) => {
+
+      switch(first_move) {
+        case 'x':
+          e.innerText = 'x';
+          first_move = 'o';
+          break;
+        case 'o':
+        e.innerText = 'o';
+        first_move = 'x';
+        break;
+      }
+    }
+  
+  const player_turn = (e) => {
     // Return players turn
+    let x_o = e
+    if (x_o == 'x') { return x_o = 'o' }
+    if (x_o == 'o') { return x_o = 'x' }
   }
-    return {reset_board, update_board};
+  
+  return {reset_board, update_board, player_turn};
 })();
 
-
-
-player_controller = () => {
-  const player_turn = function() {
-    let player_1 = document.getElementById("player-1");
-    let player_2 = document.getElementById("player-2");
-  }
-
-  
-}
 
