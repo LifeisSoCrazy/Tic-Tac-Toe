b878a6c21e3baed0ada9554aa507acc98353e849
LifeisSoCrazy <rahmanmohammed51@gmail.com>
2022-10-16 20:51:38 -0400

    After different deletions and insertions I got the logic done, although it should be tweaked.

--- main.js
+++ main.js
@@ -22,53 +22,6 @@ const positions = Array.from(document.getElementsByClassName("position"));
 const reset = document.getElementsByClassName("reset")
 const choices = Array.from(document.querySelectorAll(".choice"));
 
-let x = []
-let o = []
-
-
-
-positions.forEach(position => {
-  position.addEventListener('click', () => {
-    if(position.innerText == '') {
-      game_board.update_board(position);
-      if (position.innerText == 'x') {
-        x.push(position.id);
-        x.sort()
-        console.log(x)
-  
-        if(x.length >= 3) {
-          const winning_combinations = logic.winning_combinations()
-          const check_for_win = function check_for_win(winning_combinations, x) {
-            return winning_combinations.some(combination => {
-              return combination.every(index => {
-                return x == combination
-              }
-              )
-            }
-            )
-          }
-          check_for_win()
-        }
-      }
-
-      if (position.innerText == 'o') {
-        o.push(position.id);
-        o.sort()
-      };
-    }})})
-  
-
-
-let current_move = '';
-
-choices.forEach(choice => {
-  choice.addEventListener('click', () => {
-    current_move = choice.innerText;
-    choice.disabled = true;
-  });
-})
-
-
 const game_board = (() => {
   const reset_board = () => {
     positions.forEach(position => {
@@ -97,23 +50,58 @@ const game_board = (() => {
 
 
 
-const logic = function logic() {
-  const winning_combinations = () => [
-    'A1', 'A2', 'A3',
-    'B1', 'B2', 'B3',
-    'C1', 'C2', 'C3',
+const winning_combinations = 
+[
+  ['A1', 'A2', 'A3'],
+  ['B1', 'B2', 'B3'],
+  ['C1', 'C2', 'C3'],
 
-    'A1', 'B1', 'C1',
-    'A2', 'B2', 'C2',
-    'A3', 'B3', 'C3',
+  ['A1', 'B1', 'C1'],
+  ['A2', 'B2', 'C2'],
+  ['A3', 'B3', 'C3'],
 
-    'A1', 'B2', 'C3',
-    'A3', 'B2', 'C1',
-  ];
-  const check_for_win = () => {
-    // Will compare the values determined to the winning_combinations and return true or false
-    return x
+  ['A1', 'B2', 'C3'],
+  ['A3', 'B2', 'C1'],
+];
+
+let x = [];
+let o = [];
+
+
+const logic = (player_choice) => {
+  for (let i=0; i < winning_combinations.length; i++) {
+    winning_combinations.forEach(combination => {
+      if (JSON.stringify(combination) === JSON.stringify(player_choice)) {
+        console.log(`${str(player_choice)} wins`)
+      }
+    })
   }
-  return { winning_combinations, check_for_win }
-}();
+}
 
+positions.forEach(position => {
+  position.addEventListener('click', () => {
+    if(position.innerText == '') {
+      game_board.update_board(position);
+      if (position.innerText == 'x') {
+        x.push(position.id);
+        x.sort();
+        logic(x);
+      }
+      if (position.innerText == 'o') {
+        o.push(position.id);
+        o.sort()
+        logic(x);
+
+      };
+    }})});
+  
+
+
+let current_move = '';
+
+choices.forEach(choice => {
+  choice.addEventListener('click', () => {
+    current_move = choice.innerText;
+    choice.disabled = true;
+  });
+})
